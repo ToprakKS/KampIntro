9e495a5666cfaaf85db128106be3f4b22b66850d
MONSTER <MONSTER@DESKTOP-0HS3G36>
2021-01-16 23:54:58 +0300

    16 Ocak Ders Sonu

---
+++ KampIntro/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety-tip güvenliği
+            // do not repeat yourself - kendini tekrarlama
+            // deger tutucu kategoriEtiketi, alias
+
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMii = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+
+            if (sistemeGirisYapmisMii == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+            
+        }
+    }
+}
