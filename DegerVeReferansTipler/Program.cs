4642d3cbae256f3667b4511a37a97ea838641b43
MONSTER <MONSTER@DESKTOP-0HS3G36>
2021-01-20 23:51:20 +0300

    20 Ocak Ders Sonu

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -8,7 +8,9 @@ namespace DegerVeReferansTipler
         {
             Console.WriteLine("Hello World!");
 
-            int sayi1 = 10;
+            int sayi1 = 10;  
+
+
             int sayi2 = 30;
             sayi1 = sayi2;
             sayi2 = 65;
