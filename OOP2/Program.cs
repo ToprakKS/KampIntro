cd00132521deee166469de31d4a5c9472b119814
MONSTER <MONSTER@DESKTOP-0HS3G36>
2021-01-23 23:53:08 +0300

    23 Ocak Ders Sonu

---
+++ OOP2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+
+            musteri1.MusteriNo = "123456";
+            musteri1.Adi = "Engin";
+            musteri1.Soyadi = "Demiroğ";
+            musteri1.TcNo = "12334241251";
+            musteri1.Id = 1;
+
+
+            //Kodlama.io
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.MusteriNo = "542234";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "1234325121";
+            musteri2.Id = 2;
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+            
+            
+            
+        }
+    }
+}
+
+//musteri1.Adi = "Toprak";
+//musteri1.Soyadi = "Sevim";
+//musteri1.Id = 1;
+//musteri1.TcNo = "21231214124";
+//musteri1.MusteriNo = "123456";
+//musteri1.SirketAdi = "?";
+
+//Gerçek Müşteri - Tüzel Müşteri
+//SOLID
